58900004ab88261215115a4484ea217c805df129
JK1320 <kaurjus1@gmail.com>
2021-04-14 21:27:57 +0100

    Complete - Level 4

--- src/App.js
+++ src/App.js
@@ -2,45 +2,74 @@ import {useState, useEffect} from "react";
 import "./App.css";
 import countriesAll from "./countriesAll";
 import CountryInfo from "./CountryInfo";
+import CountryInfoOnClick from "./CountryInfoOnClick";
 import SelectRegion from "./SelectRegion";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [countries, setCountries] = useState(countriesAll);
   const [selectValue, setSelectValue] = useState("");
-
+  const [clickedCountry, setClickedCountry] = useState();
 
   useEffect(() => {
-   if (inputValue === "" && selectValue === ""){
-     setCountries(countriesAll);
-   } else if (inputValue !== "" && selectValue === "") {
-     const filteredCountries = countriesAll.filter(item => item.name.toLowerCase().includes(inputValue.toLowerCase()) || item.capital.toLowerCase().includes(inputValue.toLowerCase()))
-   setCountries(filteredCountries)
+    if (inputValue === "" && selectValue === "") {
+      setCountries(countriesAll);
+    } else if (inputValue !== "" && selectValue === "") {
+      const filteredCountries = countriesAll.filter(
+        (item) =>
+          item.name.toLowerCase().includes(inputValue.toLowerCase()) ||
+          item.capital.toLowerCase().includes(inputValue.toLowerCase())
+      );
+      setCountries(filteredCountries);
     } else if (inputValue === "" && selectValue !== "") {
-      const filteredCountries = countriesAll.filter(item => item.region.toLowerCase().includes(selectValue.toLowerCase()))
-    setCountries(filteredCountries)
+      const filteredCountries = countriesAll.filter((item) =>
+        item.region.toLowerCase().includes(selectValue.toLowerCase())
+      );
+      setCountries(filteredCountries);
     } else {
-       const filteredCountries = countriesAll.filter(item => item.region.toLowerCase().includes(selectValue.toLowerCase()) && (item.name.toLowerCase().includes(inputValue.toLowerCase()) || item.capital.toLowerCase().includes(inputValue.toLowerCase())))
-    setCountries(filteredCountries)
+      const filteredCountries = countriesAll.filter(
+        (item) =>
+          item.region.toLowerCase().includes(selectValue.toLowerCase()) &&
+          (item.name.toLowerCase().includes(inputValue.toLowerCase()) ||
+            item.capital.toLowerCase().includes(inputValue.toLowerCase()))
+      );
+      setCountries(filteredCountries);
     }
-  }, [inputValue, selectValue])
+  }, [inputValue, selectValue]);
 
   return (
     <div className="App">
       <h1>Challenge Countries</h1>
-      <input
-        value={inputValue}
-        onChange={event => setInputValue(event.target.value)}
-        className="input-value"
-        placeholder="search..."
-      /> 
-      <SelectRegion selectValue={selectValue} setSelectValue={setSelectValue} />
+      {clickedCountry ? (
+        <button onClick={() => setClickedCountry()}>Back</button>
+      ) : (
+        <>
+          <input
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+            className="input-value"
+            placeholder="search..."
+          />
+          <SelectRegion
+            selectValue={selectValue}
+            setSelectValue={setSelectValue}
+          />
+        </>
+      )}
+
       <div className="container">
-       
-        {countries.map((country, index) => {
-          return <CountryInfo key={index} data={country} />
-        }
-          
+        {clickedCountry ? (
+          <CountryInfoOnClick data={clickedCountry} />
+        ) : (
+          countries.map((country, index) => {
+            return (
+              <CountryInfo
+                onClick={() => setClickedCountry(country)}
+                key={index}
+                data={country}
+              />
+            );
+          })
         )}
       </div>
     </div>
--- src/CountryInfoOnClick.js
+++ src/CountryInfoOnClick.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-
-const CountryInfo = (props) => {
+const CountryInfoOnClick = (props) => {
   return (
-    <div onClick={props.onClick} className="box">
+    <div>
       <img src={props.data.flag} alt="country flag" />
       <h3>{props.data.name}</h3>
+      <p>Native Name: {props.data.nativeName}</p>
       <p>Population: {props.data.population}</p>
       <p>Region: {props.data.region}</p>
+      <p>Sub Region: {props.data.subregion}</p>
       {props.data.capital && <p>Capital: {props.data.capital}</p>}
+      <p>Top Level Domain: {props.data.topLevelDomain}</p>
     </div>
   );
 };
 
-export default CountryInfo;
+export default CountryInfoOnClick;
